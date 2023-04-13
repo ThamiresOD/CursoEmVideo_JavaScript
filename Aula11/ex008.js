9a2b43e23772709585e56eab6ec9820cfc46cb52
Thamires Ouverney <touverneydarci@gmail.com>
2023-04-13 14:42:37 -0300

    Aula11 ex008

---
+++ Aula11/ex008.js
@@ -0,0 +1,9 @@
+let vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+// Condição simples
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade permitida. MULTADO!`)
+}
+
+console.log(`Utilize sempre o cinto de segurança!`)
