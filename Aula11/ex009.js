ce6953390783a9cd39967704ad46353e905ab1e1
Thamires Ouverney <touverneydarci@gmail.com>
2023-04-13 14:47:29 -0300

    Aula11 - ex009

---
+++ Aula11/ex009.js
@@ -0,0 +1,9 @@
+let country = 'França'
+console.log(`Vivendo em ${country} `)
+
+// Condição simples
+if (country == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
