d27dcd49f60fb0fc4cfb6f3e88555a7347b0aa3a
Thamires Ouverney <touverneydarci@gmail.com>
2023-05-04 17:02:13 -0300

    Aula13

---
+++ Aula13/ex017.js
@@ -0,0 +1,11 @@
+let c = 0
+while (c <= 5) {
+    console.log(`[WHILE] Contador = ${c}`)
+    c++
+}
+console.log(` Contador = ${c}`)
+
+do {
+    console.log(`[DO - WHILE] Contador = ${c}`)
+    c--
+} while (c >= 0)
