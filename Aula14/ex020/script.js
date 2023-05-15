1096f9493c2776fec87c251041bd05be4433e418
Thamires Ouverney <touverneydarci@gmail.com>
2023-05-15 15:40:08 -0300

    Aula14

---
+++ Aula14/ex020/script.js
@@ -0,0 +1,24 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número válido!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        for (c = 1;c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+        // while (c <= 10) {
+        //     let item = document.createElement('option')
+        //     item.text = `${n} x ${c} = ${n * c}`
+        //     item.value = `tab${c}`
+        //     tab.appendChild(item)
+        //     c++
+        // }
+    }
+}
